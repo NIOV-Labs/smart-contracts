eb99e219b74a34d55202e9de9b22b75fe7f449ac
CT Nava <ctnava97@gmail.com>
2024-05-31 21:35:04 -0700

    log fix

--- config/index.js
+++ config/index.js
@@ -12,11 +12,10 @@ class Config {
 		if (print) {
 			console.log(`|||| Gas Reporter`);
 			if (coinmarketcap === '')
-				console.log(`||||||\x1B[92m CoinMarketCap API Key Found!\x1B[39m`);
-			else
 				console.log(
 					`||||||\x1B[33m CoinMarketCap API Key Missing! Unable to report gas consumption...\x1B[39m`
 				);
+			else console.log(`||||||\x1B[92m CoinMarketCap API Key Found!\x1B[39m`);
 		}
 		this.gasReporter =
 			coinmarketcap === ''
